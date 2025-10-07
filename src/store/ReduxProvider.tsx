be8c3e4994649f026a586b5a9e558736e347c327
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-10-07 20:14:12 +0300

    feat: Create  redux provider

---
+++ src/store/ReduxProvider.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from './store';
+
+interface ReduxProviderProps {
+  children: React.ReactNode;
+}
+
+const ReduxProvider: React.FC<ReduxProviderProps> = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
+}
+
+export { ReduxProvider };
+export default ReduxProvider;
