f7d967c81f0627fd4ef8e88ca42ff7e96bbe0d49
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-10-09 17:50:46 +0300

    feat: Update some auth types and added new types also

--- src/features/auth/types/index.ts
+++ src/features/auth/types/index.ts
@@ -38,6 +38,15 @@ export interface LoginRequest {
   password: string;
 }
 
+export interface LoginResponse {
+  data: {
+    user: User,
+    token: string
+  };
+  message: string;
+  status: string;
+}
+
 export interface RegisterRequest {
   firstName: string;
   lastName: string;
@@ -47,18 +56,52 @@ export interface RegisterRequest {
   password_confirmation: string;
 }
 
-export interface OTPRequest {
-  otp: number;
+export interface RegisterResponse {
+  data: User;
+  message: string;
+  status: string;
+}
+
+export interface VerifyEmailRequest {
+  code: string;
   email: string;
 }
 
-export interface PasswordResetRequest {
+export interface VerifyEmailResponse {
+  data: {
+    user: User;
+    token: string;
+  };
+  message: string;
+  status: string;
+}
+
+export interface ResendCodeRequest {
   email: string;
 }
 
-export interface PasswordResetConfirmRequest {
+export interface ResendCodeResponse {
+  data: User;
+  message: string;
+  status: string;
+}
+
+export interface ForgetPasswordRequest {
   email: string;
-  token: string;
+}
+
+export interface ForgetPasswordResponse {
+  message: string;
+  status: string;
+}
+
+export interface ResetPasswordRequest {
+  email: string;
   password: string;
   password_confirmation: string;
+  token: string;
+}
+
+export interface ResetPasswordResponse {
+  message: string;
 }
