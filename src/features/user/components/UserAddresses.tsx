a7ffad78394c376465ec8b0f59cff639277b815d
Azooz101ezeo <asooma2005asooma@gmail.com>
2025-11-07 18:35:09 +0300

    feat: User Addresses section has been added

---
+++ src/features/user/components/UserAddresses.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, CardContent } from '@shared/components/ui/card';
+import { AddressSelector } from '@shared/components/common/AddressSelector';
+
+interface UserAddressesProps {
+  userId: number;
+}
+
+export const UserAddresses: React.FC<UserAddressesProps> = ({ userId }) => {
+
+    return (
+    <Card className="mb-6 pt-4">
+      <CardContent>
+        <AddressSelector
+          selectedAddressId={null}
+          onAddressSelect={() => {}}
+          userId={userId}
+          showAddButtons={true}
+          showEditButtons={true}
+          showDeleteButtons={true}
+          mode="display"
+        />
+      </CardContent>
+    </Card>
+  );
+}
